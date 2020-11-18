9a8cbbc39b5c642059a77707ea7624634634c7d9
Ugur Armagan <ugurarmagan93@gmail.com>
2020-11-17 20:09:51 -0600

    configures tests to work

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,12 @@
 module.exports = {
+  preset: 'ts-jest',
+  rootDir: './src',
   testEnvironment: 'node',
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node",
-  ],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx,js,jsx}',
-    '!src/**/*.d.ts',
-  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['<rootDir>/**/?(*.)(test).ts?(x)'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
+  verbose: true,
 };
